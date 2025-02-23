5dcd15b4f65d20f2a3c1ee045ffc22bb68159344
Pratiksha Darekar <113767661+PratikshaD45@users.noreply.github.com>
2025-02-23 06:34:14 +0530

    Time: 0 ms (100%), Space: 28 MB (90.13%) - LeetHub

---
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int preIndex = 0;
+    int postIndex = 0;
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        TreeNode* root= new TreeNode(preorder[preIndex++]);
+        if(root->val != postorder[postIndex]){
+            root->left = constructFromPrePost(preorder, postorder);
+        }
+        if(root->val != postorder[postIndex]){
+            root->right = constructFromPrePost(preorder, postorder);
+        }
+        postIndex++;
+        return root;
+        
+    }
+};
